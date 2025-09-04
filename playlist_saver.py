fcc3047a97c57ae7719b2351d5278451eec1e56e
Ian Rayburn <ianrayburn9@gmail.com>
2025-09-03 20:34:52 -0400

    chore: add new env var for username and add init source code

---
+++ playlist_saver.py
@@ -0,0 +1,65 @@
+import os
+import sys
+
+import pandas as pd
+import spotipy
+from dotenv import load_dotenv
+from spotipy.oauth2 import SpotifyOAuth
+
+
+def get_playlist(sp: spotipy.Spotify, playlist_id: str) -> pd.DataFrame:
+    column_list = [
+        "track_id",
+        "track_name",
+        "artist(s)",
+        "album_name",
+        "added_at",
+        "duration_ms",
+        "popularity",
+    ]
+    df = pd.DataFrame(columns=column_list)  # type: ignore
+
+    playlist = sp.playlist_items(playlist_id)
+    for i, song in enumerate(playlist["items"]):  # type: ignore
+        df.loc[i] = [
+            song["track_id"],
+            song["track_name"],
+            song["artist(s)"],
+            song["album_name"],
+            song["added_at"],
+            song["duration_ms"],
+            song["popularity"],
+        ]
+
+    return df
+
+
+def auth_spotipy() -> spotipy.Spotify:
+    load_dotenv()
+
+    client_id = os.getenv("SPOTIPY_CLIENT_ID")
+    client_secret = os.getenv("SPOTIPY_CLIENT_SECRET")
+    redirect_uri = os.getenv("SPOTIPY_REDIRECT_URI")
+    scope = "playlist-read-private playlist-read-collaborative"
+
+    # Make sure all env vars are loaded
+    if not all([client_id, client_secret, redirect_uri]):
+        print(
+            "[ERROR]: .env file was not able to be loaded\n"
+            "Please make sure you created a .env file and entered the ALL the following:\n"
+            "\tSPOTIPY_CLIENT_ID=your-id\n"
+            "\tSPOTIPY_CLIENT_SECRET=your-secret\n"
+            "\tSPOTIPY_REDIRECT_URL=your-url\n"
+        )
+
+        sys.exit(1)
+
+    return spotipy.Spotify(
+        auth_manager=SpotifyOAuth(
+            client_id=client_id,
+            client_secret=client_secret,
+            redirect_uri=redirect_uri,
+            scope=scope,
+            open_browser=True,
+        )
+    )
---
+++ test.py
@@ -0,0 +1,21 @@
+from auth import auth_spotipy
+
+
+def main():
+    sp = auth_spotipy()
+
+    playlists = sp.user_playlists("lea8z8w6s6rpdigkjh2rj789a")
+
+    while playlists:
+        for i, playlist in enumerate(playlists["items"]):
+            print(
+                f"{i + 1 + playlists['offset']:4d} {playlist['uri']} {playlist['name']}"
+            )
+        if playlists["next"]:
+            playlists = sp.next(playlists)
+        else:
+            playlists = None
+
+
+if __name__ == "__main__":
+    main()
